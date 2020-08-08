f66a9fe13e5c375d86a0bad54fdd73e30491a5ef
Metal Gear Sloth <metalgearsloth@gmail.com>
2020-08-08 20:10:31 +1000

    Startin'

---
+++ Content.Server/GameObjects/Components/Movement/PortalComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Server.GameObjects.Components.Movement
+{
+    public class PortalComponent
+    {
+        
+    }
+}
--- Content.Server/GameObjects/EntitySystems/PortalSystem.cs
+++ Content.Server/GameObjects/EntitySystems/PortalSystem.cs
@@ -1,25 +1,18 @@
 using Content.Server.GameObjects.Components.Movement;
-using JetBrains.Annotations;
-using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
 
-namespace Content.Server.Interfaces.GameObjects.Components.Interaction
+namespace Content.Server.GameObjects.EntitySystems
 {
-    [UsedImplicitly]
-    public class PortalSystem : EntitySystem
+    internal sealed class PortalSystem : EntitySystem
     {
-        public override void Initialize()
+        /// <summary>
+        ///     Portal connections are 1-way which means you can have circular loops with multiple portals.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        public void Connect(PortalComponent from, PortalComponent to)
         {
-            EntityQuery = new TypeEntityQuery(typeof(ServerPortalComponent));
-        }
-
-        public override void Update(float frameTime)
-        {
-            foreach (var entity in RelevantEntities)
-            {
-                var comp = entity.GetComponent<ServerPortalComponent>();
-                comp.OnUpdate();
-            }
+            from.Destination = to.Owner;
         }
     }
 }
