5e7e61f0af7d5a60cc326e1e6ca97501a69a0264
Metal Gear Sloth <metalgearsloth@gmail.com>
2020-08-30 19:17:49 +1000

    Player light system
    This was talked about a long time ago in discord so I assume it's still intended. Adds a small ambient light to players (client-side only for their particular entity) just so they can see themselves in game.

---
+++ Content.Client/GameObjects/EntitySystems/LocalPlayerLightSystem.cs
@@ -0,0 +1,44 @@
+#nullable enable
+using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+using Robust.Client.GameObjects.EntitySystems;
+using Robust.Shared.GameObjects.Systems;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Maths;
+
+namespace Content.Client.GameObjects.EntitySystems
+{
+    /// <summary>
+    ///     Provides a small ambient light player-side only for their attached entity
+    /// </summary>
+    [UsedImplicitly]
+    internal sealed class LocalPlayerLightSystem : EntitySystem
+    {
+        private IEntity? _lightEntity = null;
+        
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<PlayerAttachSysMessage>(HandlePlayerAttached);
+        }
+
+        private void HandlePlayerAttached(PlayerAttachSysMessage message)
+        {
+            if (_lightEntity?.HasComponent<PointLightComponent>() == true)
+            {
+                _lightEntity?.RemoveComponent<PointLightComponent>();
+            }
+            
+            if (message.AttachedEntity == null || message.AttachedEntity.HasComponent<PointLightComponent>()) 
+                return;
+            
+            _lightEntity = message.AttachedEntity;
+            var pointLight = _lightEntity.AddComponent<PointLightComponent>();
+
+            pointLight.Enabled = true;
+            pointLight.Color = new Color(89, 90, 150);
+            pointLight.Energy = 0.5f;
+            pointLight.Radius = 2.0f;
+        }
+    }
+}
