fd66a21c237e693e5f7de46cd12cf81cf895271b
Metal Gear Sloth <metalgearsloth@gmail.com>
2020-10-22 20:58:39 +1100

    Someone else

---
+++ Content.Client/Audio/SpaceAudioEffect.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared.Maps;
+using JetBrains.Annotations;
+using Robust.Client.Audio;
+using Robust.Client.Interfaces.Graphics;
+using Robust.Shared.Audio;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Map;
+
+namespace Content.Client.Audio
+{
+    [UsedImplicitly]
+    public sealed class SpaceAudioEffect : IAudioEffect
+    {
+        public bool TrySetEntityEffect(IClydeAudioSource source, IEntity entity)
+        {
+            var tile = entity.Transform.Coordinates.GetTileRef();
+            if (tile != null && !tile.Value.Tile.IsEmpty) return false;
+            source.SetAudioEffect(AudioEffect.Space);
+            return true;
+        }
+
+        public bool TrySetCoordsEffect(IClydeAudioSource source, EntityCoordinates coordinates)
+        {
+            var tile = coordinates.GetTileRef();
+            if (tile != null && !tile.Value.Tile.IsEmpty) return false;
+            source.SetAudioEffect(AudioEffect.Space);
+            return true;
+        }
+    }
+}
