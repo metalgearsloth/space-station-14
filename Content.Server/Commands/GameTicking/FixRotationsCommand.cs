180c4ac957d159143ffb7c4630c0f866e61b72ae
Metal Gear Sloth <metalgearsloth@gmail.com>
2021-02-18 22:27:57 +1100

    Allow arbitrary angles for fixrotations

--- Content.Server/Commands/GameTicking/FixRotationsCommand.cs
+++ Content.Server/Commands/GameTicking/FixRotationsCommand.cs
@@ -1,6 +1,8 @@
-﻿using Content.Server.Administration;
+﻿#nullable enable
+using Content.Server.Administration;
 using Content.Shared.GameObjects.Components;
 using Content.Server.GameObjects.Components;
+using Content.Server.GameObjects.Components.Items.Storage;
 using Content.Shared.Administration;
 using Robust.Server.Player;
 using Robust.Shared.Console;
@@ -8,6 +10,7 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Map;
 using Robust.Shared.Maths;
+using Robust.Shared.Utility;
 
 namespace Content.Server.Commands.GameTicking
 {
@@ -16,30 +19,43 @@ namespace Content.Server.Commands.GameTicking
     {
         // ReSharper disable once StringLiteralTypo
         public string Command => "fixrotations";
-        public string Description => "Sets the rotation of all occluders, low walls and windows to south.";
-        public string Help => $"Usage: {Command} <gridId> | {Command}";
+        public string Description => "Sets the rotation of all occluders, low walls and windows to the specified angle (in degrees).";
+        public string Help => $"Usage: {Command} <gridId> <angle> | {Command} <angle>";
 
         public void Execute(IConsoleShell shell, string argsOther, string[] args)
         {
             var player = shell.Player as IPlayerSession;
 
             GridId gridId;
+            int angleDegrees;
 
             switch (args.Length)
             {
-                case 0:
+                case 1:
                     if (player?.AttachedEntity == null)
                     {
                         shell.WriteLine("Only a player can run this command.");
                         return;
                     }
 
+                    if (!int.TryParse(args[0], out angleDegrees))
+                    {
+                        shell.WriteLine($"{args[0]} is not a valid angle.");
+                        return;
+                    }
+
                     gridId = player.AttachedEntity.Transform.GridID;
                     break;
-                case 1:
+                case 2:
                     if (!int.TryParse(args[0], out var id))
                     {
                         shell.WriteLine($"{args[0]} is not a valid integer.");
+                        return;
+                    }
+
+                    if (!int.TryParse(args[1], out angleDegrees))
+                    {
+                        shell.WriteLine($"{args[1]} is not a valid angle.");
                         return;
                     }
 
@@ -50,6 +66,7 @@ namespace Content.Server.Commands.GameTicking
                     return;
             }
 
+            var angle = Angle.FromDegrees(angleDegrees);
             var mapManager = IoCManager.Resolve<IMapManager>();
             if (!mapManager.TryGetGrid(gridId, out var grid))
             {
@@ -74,18 +91,26 @@ namespace Content.Server.Commands.GameTicking
 
                 var valid = false;
 
+                if (childEntity.HasComponent<IMapGridComponent>() || childEntity.HasComponent<MapComponent>())
+                {
+                    continue;
+                }
+
                 valid |= childEntity.HasComponent<OccluderComponent>();
                 valid |= childEntity.HasComponent<SharedCanBuildWindowOnTopComponent>();
                 valid |= childEntity.HasComponent<WindowComponent>();
+                valid |= childEntity.HasComponent<ServerStorageComponent>();
 
                 if (!valid)
                 {
                     continue;
                 }
 
-                if (childEntity.Transform.LocalRotation != Angle.South)
+                if (childEntity.Transform.LocalRotation != angle)
                 {
-                    childEntity.Transform.LocalRotation = Angle.South;
+                    childEntity.Transform.LocalRotation = angle;
+                    var localPos = childEntity.Transform.LocalPosition;
+                    DebugTools.Assert(!float.IsNaN(localPos.X) && !float.IsNaN(localPos.Y));
                     changed++;
                 }
             }
