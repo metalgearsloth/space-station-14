111986752550f6617b9c032c44281a11c4b4fecb
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-05-06 00:26:34 +1000

    Bolt visualizer

--- Content.Client/GameObjects/Components/Weapons/Gun/GunBoltVisualizer.cs
+++ Content.Client/GameObjects/Components/Weapons/Gun/GunBoltVisualizer.cs
@@ -1,14 +1,29 @@
+using Content.Shared.GameObjects.Components.Weapons.Guns;
 using Robust.Client.GameObjects;
+using Robust.Shared.GameObjects;
 
 namespace Content.Client.GameObjects.Components.Weapons.Gun
 {
     public sealed class GunBoltVisualizer : AppearanceVisualizer
     {
+        public override void InitializeEntity(IEntity entity)
+        {
+            base.InitializeEntity(entity);
+            if (!entity.TryGetComponent(out ISpriteComponent? spriteComponent)) return;
+
+            spriteComponent.LayerSetState(GunVisuals.BoltClosed, "bolt-closed");
+        }
+
         public override void OnChangeData(AppearanceComponent component)
         {
             base.OnChangeData(component);
             if (!component.Owner.TryGetComponent(out ISpriteComponent? spriteComponent)) return;
-            // TODO: Visuals here default to closed if (component.TryGetData())
+            if (!component.TryGetData(GunVisuals.BoltClosed, out bool boltClosed))
+            {
+                return;
+            }
+
+            spriteComponent.LayerSetState(GunVisuals.BoltClosed, boltClosed ? "bolt-closed" : "bolt-open");
         }
     }
 }
