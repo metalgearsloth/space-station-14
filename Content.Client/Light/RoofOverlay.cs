b88812f36eceeb594cd5eb6387501983a0c9e27a
metalgearsloth <comedian_vs_clown@hotmail.com>
2025-07-14 23:33:52 +1000

    Don't draw rooves on implicit space tiles

--- Content.Client/Light/RoofOverlay.cs
+++ Content.Client/Light/RoofOverlay.cs
@@ -21,6 +21,7 @@ public sealed class RoofOverlay : Overlay
     private readonly SharedMapSystem _mapSystem;
     private readonly SharedRoofSystem _roof = default!;
     private readonly SharedTransformSystem _xformSystem;
+    private readonly TurfSystem _turf = default!;
 
     private List<Entity<MapGridComponent>> _grids = new();
 
@@ -37,6 +38,7 @@ public sealed class RoofOverlay : Overlay
         _mapSystem = _entManager.System<SharedMapSystem>();
         _roof = _entManager.System<SharedRoofSystem>();
         _xformSystem = _entManager.System<SharedTransformSystem>();
+        _turf = _entManager.System<TurfSystem>();
 
         ZIndex = ContentZIndex;
     }
@@ -81,6 +83,10 @@ public sealed class RoofOverlay : Overlay
 
                     while (tileEnumerator.MoveNext(out var tileRef))
                     {
+                        // E.g. lattice, light should still go through.
+                        if (_turf.IsSpace(tileRef))
+                            continue;
+
                         var local = _lookup.GetLocalBounds(tileRef, grid.Comp.TileSize);
                         worldHandle.DrawRect(local, color);
                     }
